71fe142f59dac1df8980c2ee623d8666fa73afa4
Seyyid Ahmed Doğan <seyahdoo@gmail.com>
2021-06-07 14:59:35 +0300

    feat laps component

---
+++ Assets/Laps/Runtime/ConsoleLogComponent.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LapsRuntime {
+    public class ConsoleLogComponent : LapsComponent {
+
+        public void HandleInput(object data) {
+            
+        }
+        
+        
+    }
+}
--- Assets/Laps/Runtime/LapsComponent.cs
+++ Assets/Laps/Runtime/LapsComponent.cs
@@ -1,9 +1,31 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace LapsRuntime {
     public class LapsComponent : MonoBehaviour {
+        public List<Connection> connections = new List<Connection>();
+        protected object FireOutput(int slotId, object parameter, int depth = 0) {
+            return null;
+        }
+        protected IEnumerable<object> FireOutputAdvanced() {
+            
+            return null;
+        }
 
+        protected virtual object HandleInput(int slotId) {
+            return null;
+        }
+    }
+    [Serializable]
+    public struct Connection {
+        public LapsComponent targetComponent;
+        public int sourceSlotId;
+        public int targetSlotId;
+    }
+    public struct Slot {
+        public string name;
+        public Type parameterType;
+        public Type returnType;
     }
 }
