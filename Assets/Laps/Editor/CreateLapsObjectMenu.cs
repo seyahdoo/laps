f08fe64add8c37178e22c05015dc6686f8f99842
Seyyid Ahmed Doğan <seyahdoo@gmail.com>
2021-06-07 11:28:07 +0300

    refactor create laps object menu

--- Assets/Laps/Editor/CreateLapsObjectMenu.cs
+++ Assets/Laps/Editor/CreateLapsObjectMenu.cs
@@ -7,17 +7,15 @@ public class CreateLapsObjectMenu : Editor {
         SceneView.duringSceneGui += SceneGUI;
     }
     private static void SceneGUI (SceneView sceneView) {
-        if (Event.current.isKey) {
-            if (Event.current.keyCode == KeyCode.Space) {
-                Event.current.Use();
-                var menu = new GenericMenu();
-                menu.AddItem(new GUIContent("hello/Item 1"), false, Callback, "hello");
-                menu.AddItem(new GUIContent("hello/Item 2"), false, Callback, 2);
-                menu.AddItem(new GUIContent("hello/Item 3"), false, Callback, 2);
-                menu.AddItem(new GUIContent("Item 4"), false, Callback, 2);
-                menu.AddItem(new GUIContent("Item 5"), false, Callback, 2);
-                menu.ShowAsContext();
-            }
+        if (Event.current.isKey && Event.current.keyCode == KeyCode.Space) {
+            Event.current.Use();
+            var menu = new GenericMenu();
+            menu.AddItem(new GUIContent("hello/Item 1"), false, Callback, "hello");
+            menu.AddItem(new GUIContent("hello/Item 2"), false, Callback, 2);
+            menu.AddItem(new GUIContent("hello/Item 3"), false, Callback, 2);
+            menu.AddItem(new GUIContent("Item 4"), false, Callback, 2);
+            menu.AddItem(new GUIContent("Item 5"), false, Callback, 2);
+            menu.ShowAsContext();
         }
     }
     private static void Callback (object obj) {
