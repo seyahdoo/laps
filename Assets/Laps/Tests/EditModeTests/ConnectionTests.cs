4b6b91c2f0b09cd05d1b29440dff16d6ad47b6fa
Seyyid Ahmed Doğan <seyahdoo@gmail.com>
2021-06-09 14:25:39 +0300

    test connection, define some tests

---
+++ Assets/Laps/Tests/EditModeTests/ConnectionTests.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace LapsEditModeTests {
+    public class ConnectionTests {
+        [Test]
+        public void ConnectBasic() { Assert.Fail("Test not implemented yet"); }
+        [Test]
+        public void DisconnectBasic() { Assert.Fail("Test not implemented yet"); }
+        [Test]
+        public void RecursiveLoopExitsAtSomePointAndLogsErrorWithFireOutputBasic() { Assert.Fail("Test not implemented yet"); }
+        [Test]
+        public void RecursiveLoopExitsAtSomePointAndLogsErrorWithFireOutputAdvanced() { Assert.Fail("Test not implemented yet"); }
+        [Test]
+        public void FireOutputBasicCallsHandleInputOnConnectedObjects() { Assert.Fail("Test not implemented yet"); }
+        [Test]
+        public void FireOutputAdvancedCallsHandleInputOnConnectedObjectsAsMoveNextCalled() { Assert.Fail("Test not implemented yet"); }
+        [Test]
+        public void TryingToConnectSameConnectionTwiceDoesNotCreateTwoConnections() { Assert.Fail("Test not implemented yet"); }
+        [Test]
+        public void CanDisconnectLastConnectionOfParticularSlot() { Assert.Fail("Test not implemented yet"); }
+    }
+}
