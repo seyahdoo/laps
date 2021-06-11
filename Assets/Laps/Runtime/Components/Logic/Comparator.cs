e8b2c38be8328c1cd15c8b7a06086cfac0230cc9
Seyyid Ahmed Doğan <seyahdoo@gmail.com>
2021-06-11 10:57:37 +0300

    feat comparator

--- Assets/Laps/Runtime/Components/Logic/Comparator.cs
+++ Assets/Laps/Runtime/Components/Logic/Comparator.cs
@@ -1,10 +1,77 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
-using UnityEngine;
 
-namespace LapsRuntime
-{
-    public class Comparator : MonoBehaviour {
-
+namespace LapsRuntime {
+    public class Comparator : LapsComponent {
+        public enum ComparisonMode {
+            LessThan = 0,
+            LessEqual = 1,
+            GraterThan = 2,
+            GraterEqual = 3,
+            Equals = 4,
+            NotEquals = 5,
+        }
+        public float a;
+        public ComparisonMode comparisonMode;
+        public float b;
+        public bool triggerOnAwake = false;
+        public bool triggerOnValueChange = true;
+        public bool triggerOnResultChange = true;
+        private bool _lastTriggerResult = false;
+        public void Awake() {
+            if (triggerOnAwake) {
+                Trigger();
+            }
+        }
+        protected override object HandleInput(int slotId, object parameter, LapsComponent eventSource) {
+            switch (slotId) {
+                case 0: a = (float) parameter; OnValueChange(); return null;
+                case 1: b = (float) parameter; OnValueChange(); return null;
+                case 2: Trigger(); return null;
+            }
+            return null;
+        }
+        public void OnValueChange() {
+            if (triggerOnValueChange) {
+                Trigger();
+                return;
+            }
+            if (triggerOnResultChange) {
+                var currentResult = Evaluate();
+                if (currentResult != _lastTriggerResult) {
+                    Trigger();
+                    return;
+                }
+            }
+        }
+        public void Trigger() {
+            _lastTriggerResult = Evaluate();
+            if (_lastTriggerResult) {
+                FireOutput(0);
+            }
+            else {
+                FireOutput(1);
+            }
+        }
+        private bool Evaluate() {
+            switch (comparisonMode) {
+                case ComparisonMode.Equals:      return Math.Abs(a - b) < 0.01f;
+                case ComparisonMode.NotEquals:   return Math.Abs(a - b) > 0.01f;
+                case ComparisonMode.GraterEqual: return a >= b;
+                case ComparisonMode.GraterThan:  return a > b;
+                case ComparisonMode.LessEqual:   return a <= b;
+                case ComparisonMode.LessThan:    return a < b;
+                default:                         return false;
+            }
+        }
+        public override void GetInputSlots(List<LogicSlot> slots) {
+            slots.Add(new LogicSlot("set a", 0));            
+            slots.Add(new LogicSlot("set b", 1));
+            slots.Add(new LogicSlot("trigger", 2));
+        }
+        public override void GetOutputSlots(List<LogicSlot> slots) {
+            slots.Add(new LogicSlot("true", 0));
+            slots.Add(new LogicSlot("false", 1));
+        }
     }
 }
---
+++ Assets/Laps/Tests/EditModeTests/ComponentTests/ComparatorTests.cs
@@ -0,0 +1,179 @@
+using LapsRuntime;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace LapsEditModeTests {
+    public class ComparatorTests {
+        private Comparator _comparator;
+        private TestComponent _testerTrue;
+        private TestComponent _testerFalse;
+        [SetUp]
+        public void Setup() {
+            _comparator = new GameObject().AddComponent<Comparator>();
+            _testerTrue = new GameObject().AddComponent<TestComponent>();
+            _testerFalse = new GameObject().AddComponent<TestComponent>();
+            LapsEditor.LapsEditor.instance.lapsEditorLogicModule.Connect(_comparator, 0, _testerTrue, 0);
+            LapsEditor.LapsEditor.instance.lapsEditorLogicModule.Connect(_comparator, 1, _testerFalse, 0);
+        }
+        [Test]
+        public void ComparatorGraterThan() {
+            _comparator.a = 10f;
+            _comparator.b = 5f;
+            _comparator.comparisonMode = Comparator.ComparisonMode.GraterThan;
+            _comparator.Trigger();
+            Assert.AreEqual(1, _testerTrue.inputCallCount);
+            Assert.AreEqual(0, _testerFalse.inputCallCount);
+        }
+        [Test]
+        public void ComparatorGraterEquals() {
+            _comparator.a = 10f;
+            _comparator.b = 5f;
+            _comparator.comparisonMode = Comparator.ComparisonMode.GraterEqual;
+            _comparator.Trigger();
+            Assert.AreEqual(1, _testerTrue.inputCallCount);
+            Assert.AreEqual(0, _testerFalse.inputCallCount);
+            
+            _comparator.a = 10f;
+            _comparator.b = 10f;
+            _comparator.Trigger();
+            Assert.AreEqual(2, _testerTrue.inputCallCount);
+            Assert.AreEqual(0, _testerFalse.inputCallCount);
+
+            _comparator.a = 10f;
+            _comparator.b = 11f;
+            _comparator.Trigger();
+            Assert.AreEqual(2, _testerTrue.inputCallCount);
+            Assert.AreEqual(1, _testerFalse.inputCallCount);
+        }
+        [Test]
+        public void ComparatorLessThan() {
+            _comparator.a = 10f;
+            _comparator.b = 5f;
+            _comparator.comparisonMode = Comparator.ComparisonMode.LessThan;
+            _comparator.Trigger();
+            Assert.AreEqual(0, _testerTrue.inputCallCount);
+            Assert.AreEqual(1, _testerFalse.inputCallCount);
+            
+            _comparator.a = 10f;
+            _comparator.b = 10f;
+            _comparator.comparisonMode = Comparator.ComparisonMode.LessThan;
+            _comparator.Trigger();
+            Assert.AreEqual(0, _testerTrue.inputCallCount);
+            Assert.AreEqual(2, _testerFalse.inputCallCount);
+        }
+        [Test]
+        public void ComparatorLessEquals() {
+            _comparator.a = 10f;
+            _comparator.b = 5f;
+            _comparator.comparisonMode = Comparator.ComparisonMode.LessEqual;
+            _comparator.Trigger();
+            Assert.AreEqual(0, _testerTrue.inputCallCount);
+            Assert.AreEqual(1, _testerFalse.inputCallCount);
+            
+            _comparator.a = 10f;
+            _comparator.b = 10f;
+            _comparator.comparisonMode = Comparator.ComparisonMode.LessEqual;
+            _comparator.Trigger();
+            Assert.AreEqual(1, _testerTrue.inputCallCount);
+            Assert.AreEqual(1, _testerFalse.inputCallCount);
+        }
+        [Test]
+        public void ComparatorEquals() {
+            _comparator.a = 10f;
+            _comparator.b = 10f;
+            _comparator.comparisonMode = Comparator.ComparisonMode.Equals;
+            _comparator.Trigger();
+            Assert.AreEqual(1, _testerTrue.inputCallCount);
+            Assert.AreEqual(0, _testerFalse.inputCallCount);
+            
+            _comparator.a = 10f;
+            _comparator.b = 5f;
+            _comparator.comparisonMode = Comparator.ComparisonMode.Equals;
+            _comparator.Trigger();
+            Assert.AreEqual(1, _testerTrue.inputCallCount);
+            Assert.AreEqual(1, _testerFalse.inputCallCount);
+        }
+        [Test]
+        public void ComparatorEqualsTolerance() {
+            _comparator.a = 10f;
+            _comparator.b = 10.0001f;
+            _comparator.comparisonMode = Comparator.ComparisonMode.Equals;
+            _comparator.Trigger();
+            Assert.AreEqual(1, _testerTrue.inputCallCount);
+            Assert.AreEqual(0, _testerFalse.inputCallCount);
+        }
+        [Test]
+        public void ComparatorNotEquals() {
+            _comparator.a = 10f;
+            _comparator.b = 5f;
+            _comparator.comparisonMode = Comparator.ComparisonMode.NotEquals;
+            _comparator.Trigger();
+            Assert.AreEqual(1, _testerTrue.inputCallCount);
+            Assert.AreEqual(0, _testerFalse.inputCallCount);
+            
+            _comparator.a = 10f;
+            _comparator.b = 10f;
+            _comparator.comparisonMode = Comparator.ComparisonMode.NotEquals;
+            _comparator.Trigger();
+            Assert.AreEqual(1, _testerTrue.inputCallCount);
+            Assert.AreEqual(1, _testerFalse.inputCallCount);
+        }
+        [Test]
+        public void ComparatorNotEqualsTolerance() {
+            _comparator.a = 10f;
+            _comparator.b = 10.0001f;
+            _comparator.comparisonMode = Comparator.ComparisonMode.NotEquals;
+            _comparator.Trigger();
+            Assert.AreEqual(0, _testerTrue.inputCallCount);
+            Assert.AreEqual(1, _testerFalse.inputCallCount);
+        }
+        [Test]
+        public void ComparatorTriggerOnAwake() {
+            _comparator.a = 10f;
+            _comparator.b = 5f;
+            _comparator.comparisonMode = Comparator.ComparisonMode.GraterThan;
+            _comparator.triggerOnAwake = true;
+            _comparator.Awake();
+            Assert.AreEqual(1, _testerTrue.inputCallCount);
+            Assert.AreEqual(0, _testerFalse.inputCallCount);
+        }
+        [Test]
+        public void ComparatorTriggerOnResultChange() {
+            _comparator.a = 10f;
+            _comparator.b = 5f;
+            _comparator.comparisonMode = Comparator.ComparisonMode.GraterThan;
+            _comparator.triggerOnResultChange = true;
+            _comparator.triggerOnValueChange = false;
+            _comparator.Trigger();
+            Assert.AreEqual(1, _testerTrue.inputCallCount);
+            Assert.AreEqual(0, _testerFalse.inputCallCount);
+            _comparator.b = 7f;
+            _comparator.OnValueChange();
+            Assert.AreEqual(1, _testerTrue.inputCallCount);
+            Assert.AreEqual(0, _testerFalse.inputCallCount);
+            _comparator.b = 15f;
+            _comparator.OnValueChange();
+            Assert.AreEqual(1, _testerTrue.inputCallCount);
+            Assert.AreEqual(1, _testerFalse.inputCallCount);
+        }
+        [Test]
+        public void ComparatorTriggerOnValueChange() {
+            _comparator.a = 10f;
+            _comparator.b = 5f;
+            _comparator.comparisonMode = Comparator.ComparisonMode.GraterThan;
+            _comparator.triggerOnResultChange = false;
+            _comparator.triggerOnValueChange = true;
+            _comparator.Trigger();
+            Assert.AreEqual(1, _testerTrue.inputCallCount);
+            Assert.AreEqual(0, _testerFalse.inputCallCount);
+            _comparator.b = 5f;
+            _comparator.OnValueChange();
+            Assert.AreEqual(2, _testerTrue.inputCallCount);
+            Assert.AreEqual(0, _testerFalse.inputCallCount);
+            _comparator.b = 15f;
+            _comparator.OnValueChange();
+            Assert.AreEqual(2, _testerTrue.inputCallCount);
+            Assert.AreEqual(1, _testerFalse.inputCallCount);
+        }
+    }
+}
