c82cb3b3dccc1e3dc3f62e143a6a0b9c053b0b0b
Seyyid Ahmed Doğan <seyahdoo@gmail.com>
2021-06-12 13:32:44 +0300

    feat timer

--- Assets/Laps/Runtime/Components/Logic/Timer.cs
+++ Assets/Laps/Runtime/Components/Logic/Timer.cs
@@ -1,13 +1,41 @@
+using System;
 using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace LapsRuntime {
     public class Timer : LapsComponent {
         public List<float> times = new List<float>() {.5f};
-        public bool triggerOnAwake = false;
+        public bool startOnAwake = false;
+        public void Awake() {
+            enabled = startOnAwake;
+        }
+        private void Update() {
+            for (int i = times.Count - 1; i >= 0; i--) {
+                times[i] -= Time.deltaTime;
+                if (times[i] <= 0f) {
+                    times.RemoveAt(i);
+                    FireOutput(0);
+                }
+            }
+        }
+        public override object HandleInput(int slotId, object parameter, LapsComponent eventSource) {
+            switch (slotId) {
+                case 0:  enabled = true; return null;
+                case 1:  enabled = false; return null;
+                case 2:  SetTimer((float) parameter); return null;
+                default: return null;
+            }
+        }
+        private void SetTimer(float time) {
+            times.Clear();
+            times.Add(time);
+            enabled = true;
+        }
         public override void GetInputSlots(List<LogicSlot> slots) {
-            slots.Add(new LogicSlot("start", 1));
-            slots.Add(new LogicSlot("stop", 0));
-            slots.Add(new LogicSlot("set time", 1, typeof(float)));
+            slots.Add(new LogicSlot("start", 0));
+            slots.Add(new LogicSlot("stop", 1));
+            slots.Add(new LogicSlot("set time", 2, typeof(float)));
         }
         public override void GetOutputSlots(List<LogicSlot> slots) {
             slots.Add(new LogicSlot("trigger", 0));
--- Assets/Laps/Tests/PlayModeTests/ComponentTests/TimerTests.cs
+++ Assets/Laps/Tests/PlayModeTests/ComponentTests/TimerTests.cs
@@ -1,13 +1,106 @@
 using System.Collections;
+using System.Collections.Generic;
+using LapsEditModeTests;
+using LapsRuntime;
 using NUnit.Framework;
+using UnityEngine;
 using UnityEngine.TestTools;
 
 namespace LapsPlayModeTests {
     public class TimerTests {
+        public Timer _timer;
+        public TestComponent _test;
+        [SetUp]
+        public void Setup() {
+            _timer = new GameObject().AddComponent<Timer>();
+            _test = new GameObject().AddComponent<TestComponent>();
+            LapsEditor.LapsEditor.instance.lapsEditorLogicModule.Connect(_timer, 0, _test, 0);
+        }
         [UnityTest]
-        public IEnumerator TimerTestsWithEnumeratorPasses() {
-            yield return null;
-            Assert.Fail("test not implemented yet");
+        public IEnumerator BasicSetup() {
+            _timer.times = new List<float>() {.05f};
+            _timer.startOnAwake = true;
+            _timer.Awake();
+            Assert.AreEqual(0, _test.inputCallCount);
+            yield return new WaitForSeconds(.1f);
+            Assert.AreEqual(1, _test.inputCallCount);
+        }
+        [UnityTest]
+        public IEnumerator MoreThanOneTime() {
+            _timer.times = new List<float>() {.05f, .10f};
+            _timer.startOnAwake = true;
+            _timer.Awake();
+            Assert.AreEqual(0, _test.inputCallCount);
+            yield return new WaitForSeconds(.07f);
+            Assert.AreEqual(1, _test.inputCallCount);
+            yield return new WaitForSeconds(.05f);
+            Assert.AreEqual(2, _test.inputCallCount);
+        }
+        [UnityTest]
+        public IEnumerator TimesListUnordered() {
+            _timer.times = new List<float>() {.15f, .05f, .10f};
+            _timer.startOnAwake = true;
+            _timer.Awake();
+            Assert.AreEqual(0, _test.inputCallCount);
+            yield return new WaitForSeconds(.07f);
+            Assert.AreEqual(1, _test.inputCallCount);
+            yield return new WaitForSeconds(.04f);
+            Assert.AreEqual(2, _test.inputCallCount);
+            yield return new WaitForSeconds(.05f);
+            Assert.AreEqual(3, _test.inputCallCount);
+        }
+        [UnityTest]
+        public IEnumerator SetTime() {
+            _timer.times = new List<float>() {.15f, .05f, .10f};
+            _timer.startOnAwake = true;
+            _timer.Awake();
+            _timer.HandleInput(2, .03f, null);
+            Assert.AreEqual(0, _test.inputCallCount);
+            yield return new WaitForSeconds(.04f);
+            Assert.AreEqual(1, _test.inputCallCount);
+            yield return new WaitForSeconds(.10f);
+            Assert.AreEqual(1, _test.inputCallCount);
+        }
+        [UnityTest]
+        public IEnumerator SetTimeStartsTimer() {
+            _timer.times = new List<float>() {.15f, .05f, .10f};
+            _timer.startOnAwake = false;
+            _timer.Awake();
+            _timer.HandleInput(2, .03f, null);
+            Assert.AreEqual(0, _test.inputCallCount);
+            yield return new WaitForSeconds(.04f);
+            Assert.AreEqual(1, _test.inputCallCount);
+            yield return new WaitForSeconds(.10f);
+            Assert.AreEqual(1, _test.inputCallCount);
+        }
+        [UnityTest]
+        public IEnumerator ManualStart() {
+            _timer.times = new List<float>() {.15f, .05f, .10f};
+            _timer.startOnAwake = false;
+            _timer.Awake();
+            _timer.HandleInput(0, null, null);
+            Assert.AreEqual(0, _test.inputCallCount);
+            yield return new WaitForSeconds(.07f);
+            Assert.AreEqual(1, _test.inputCallCount);
+            yield return new WaitForSeconds(.04f);
+            Assert.AreEqual(2, _test.inputCallCount);
+            yield return new WaitForSeconds(.05f);
+            Assert.AreEqual(3, _test.inputCallCount);
+        }
+        [UnityTest]
+        public IEnumerator StopHalfway() {
+            _timer.times = new List<float>() {.15f, .05f, .10f};
+            _timer.startOnAwake = false;
+            _timer.Awake();
+            _timer.HandleInput(0, null, null);
+            Assert.AreEqual(0, _test.inputCallCount);
+            yield return new WaitForSeconds(.07f);
+            Assert.AreEqual(1, _test.inputCallCount);
+            _timer.HandleInput(1, null, null);
+            yield return new WaitForSeconds(.04f);
+            Assert.AreEqual(1, _test.inputCallCount);
+            yield return new WaitForSeconds(.05f);
+            Assert.AreEqual(1, _test.inputCallCount);
         }
     }
 }
