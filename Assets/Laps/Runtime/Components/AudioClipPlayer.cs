82b40a13fde710a024c464c6bf6c527f47dfbe65
Seyyid Ahmed Doğan <seyahdoo@gmail.com>
2021-06-15 16:12:09 +0300

    feat audo clip player unfinished

--- Assets/Laps/Runtime/Components/AudioClipPlayer.cs
+++ Assets/Laps/Runtime/Components/AudioClipPlayer.cs
@@ -1,10 +1,21 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace LapsRuntime
-{
-    public class AudioClipPlayer : MonoBehaviour {
-
+namespace LapsRuntime {
+    [RequireComponent(typeof(AudioSource))]
+    public class AudioClipPlayer : LapsComponent {
+        public List<AudioClip> startClips = new List<AudioClip>();
+        public List<AudioClip> loopClips = new List<AudioClip>();
+        public List<AudioClip> endClips = new List<AudioClip>();
+        public bool playOnAwake = false;
+        public override void GetOutputSlots(SlotList slots) {
+            slots.Add("on end", 0);
+        }
+        public override void GetInputSlots(SlotList slots) {
+            slots.Add("play", 0);
+            slots.Add("pause", 1);
+            slots.Add("stop", 2);
+            slots.Add("stop immidiately", 3);
+        }
     }
 }
---
+++ Assets/Laps/Tests/PlayModeTests/ComponentTests/AudioClipPlayerTests.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using LapsRuntime;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace LapsPlayModeTests {
+    public class AudioClipPlayerTests {
+        public AudioClipPlayer audioClipPlayer;
+        public AudioSource audioSource;
+        [SetUp]
+        public void Setup() {
+            audioClipPlayer = new GameObject().AddComponent<AudioClipPlayer>();
+            audioSource = audioClipPlayer.GetComponent<AudioSource>();
+        }
+        [TearDown]
+        public void Teardown() {
+            Object.DestroyImmediate(audioClipPlayer.gameObject);
+        }
+        [UnityTest]
+        public IEnumerator AudioSourceAwakeStatusWontGenerateAudio() {
+            Assert.Pass("cannot test this");
+            yield return null;
+        }
+        [UnityTest]
+        public IEnumerator OneCLipInStartClip() {
+            
+            yield return null;
+        }
+        [UnityTest]
+        public IEnumerator TwoClipInStartClipPicksRandom() {
+            yield return null;
+        }
+        [UnityTest]
+        public IEnumerator OnlyLoop() {
+            yield return null;
+        }
+        [UnityTest]
+        public IEnumerator StartAndLoop() {
+            yield return null;
+        }
+        [UnityTest]
+        public IEnumerator StartAndLoopAndEnd() {
+            yield return null;
+        }
+        [UnityTest]
+        public IEnumerator PauseAndContunie() {
+            yield return null;
+        }
+        [UnityTest]
+        public IEnumerator PlayOnAwake() {
+            yield return null;
+        }
+        [Test]
+        public void Slots() {
+            var slots = new SlotList();
+            audioClipPlayer.GetInputSlots(slots);
+            Assert.AreEqual(4, slots.Count);
+            Assert.AreEqual(new LogicSlot("play", 0), slots[0]);
+            Assert.AreEqual(new LogicSlot("pause", 1), slots[1]);
+            Assert.AreEqual(new LogicSlot("stop", 2), slots[2]);
+            Assert.AreEqual(new LogicSlot("stop immidiately", 3), slots[3]);
+            slots.Clear();
+            audioClipPlayer.GetOutputSlots(slots);
+            Assert.AreEqual(1, slots.Count);
+            Assert.AreEqual(new LogicSlot("on end", 0), slots[0]);
+        }
+    }
+}
