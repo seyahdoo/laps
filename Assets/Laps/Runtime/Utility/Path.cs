a463012aabfd6145743a4f0925db35b21ae28f92
Seyyid Ahmed Doğan <seyahdoo@gmail.com>
2021-06-19 13:56:41 +0300

    feat path, work in progress

--- Assets/Laps/Runtime/Utility/Path.cs
+++ Assets/Laps/Runtime/Utility/Path.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngineInternal;
 
 namespace LapsRuntime {
     [Serializable]
@@ -17,16 +18,19 @@ namespace LapsRuntime {
             private int _previousIndex;
             private int _currentIndex;
             private int _nextIndex;
+            private float _progress;
             
             public PathEnumerator(Path path) {
                 _path = path;
                 _previousIndex = 0;
                 _currentIndex = 0;
                 _nextIndex = 1;
+                _progress = 0f;
             }
             public int PreviousPointIndex => _previousIndex;
             public int CurrentPointIndex => _currentIndex;
             public int NextPointIndex => _nextIndex;
+            public Vector3 CurrentPosition => Vector3.LerpUnclamped(_path.points[_currentIndex], _path.points[_nextIndex], _progress);
             public bool GoToNextPoint() {
                 var nextIndex = GetNextIndex(_currentIndex);
                 if (nextIndex == _currentIndex) {
@@ -35,13 +39,43 @@ namespace LapsRuntime {
                 _previousIndex = _currentIndex;
                 _currentIndex = nextIndex;
                 _nextIndex = GetNextIndex(_currentIndex);
+                _progress = 0f;
                 return true;
             }
             public bool GoToPreviousPoint() {
-                throw new NotImplementedException();
+                var prevIndex = GetPrevIndex(_currentIndex);
+                if (prevIndex == _currentIndex) {
+                    if(_progress > 0) {
+                        _progress = 0f;
+                        return true;
+                    }
+                    return false;
+                }
+                _nextIndex = _currentIndex;
+                _currentIndex = prevIndex;
+                _previousIndex = GetPrevIndex(_currentIndex);
+                _progress = 1f;
+                return true;
             }
             public bool GoForward(float distance) {
-                throw new NotImplementedException();
+                int killSwitch = _path.points.Count + 1;
+                while (killSwitch-- > 0 && distance > 0) {
+                    var distanceOfCurrentAndNextPoint = Vector3.Distance(_path.points[_currentIndex], _path.points[_nextIndex]);
+                    var leftDistance = (1f - _progress) * distanceOfCurrentAndNextPoint;
+                    if (distance > leftDistance) {
+                        _progress = 0;
+                        distance -= leftDistance;
+                        if (!GoToNextPoint()) {
+                            return false;
+                        }
+                    }
+                    else {
+                        var p = distance / distanceOfCurrentAndNextPoint;
+                        _progress += p;
+                        return true;
+                    }
+                }
+                return true;
             }
             public bool GoBackward(float distance) {
                 throw new NotImplementedException();
@@ -49,6 +83,14 @@ namespace LapsRuntime {
             private int GetNextIndex(int current) {
                 if (current <= _path.points.Count - 2) {
                     return current + 1;
+                }
+                else {
+                    return current;
+                }
+            }
+            private int GetPrevIndex(int current) {
+                if (current >= 1) {
+                    return current - 1;
                 }
                 else {
                     return current;
--- Assets/Laps/Tests/EditModeTests/PathTests.cs
+++ Assets/Laps/Tests/EditModeTests/PathTests.cs
@@ -91,6 +91,80 @@ namespace LapsEditModeTests {
             Assert.AreEqual(0, e.CurrentPointIndex);
             Assert.AreEqual(1, e.NextPointIndex);
         }
+        [Test]
+        public void CurrentPointWithoutGoingToNextPoint() {
+            path.points = new List<Vector3>() {Vector3.zero, Vector3.right, Vector3.right * 2f};
+            var e = path.GetEnumerator();
+            Assert.AreEqual(Vector3.zero, e.CurrentPosition);
+            Assert.IsTrue(e.GoForward(.5f));
+            Assert.AreEqual(Vector3.right * 0.5f, e.CurrentPosition);
+            Assert.IsTrue(e.GoForward(.3f));
+            Assert.AreEqual(Vector3.right * 0.8f, e.CurrentPosition);
+        }
+        [Test]
+        public void CurrentPointWithGoingToNextPoint() {
+            path.points = new List<Vector3>() {Vector3.zero, Vector3.right, Vector3.right + Vector3.up};
+            var e = path.GetEnumerator();
+            Assert.AreEqual(Vector3.zero, e.CurrentPosition);
+            Assert.IsTrue(e.GoForward(.5f));
+            Assert.AreEqual(Vector3.right * 0.5f, e.CurrentPosition);
+            Assert.IsTrue(e.GoForward(1f));
+            Assert.AreEqual(Vector3.right + (Vector3.up * 0.5f), e.CurrentPosition);
+        }
+        [Test]
+        public void CurrentPointWithGoingToNextTwoPoint() {
+            path.points = new List<Vector3>() {Vector3.zero, Vector3.right, Vector3.right + Vector3.up, Vector3.right + Vector3.up + Vector3.right};
+            var e = path.GetEnumerator();
+            Assert.AreEqual(Vector3.zero, e.CurrentPosition);
+            Assert.IsTrue(e.GoForward(.5f));
+            Assert.AreEqual(Vector3.right * 0.5f, e.CurrentPosition);
+            Assert.IsTrue(e.GoForward(2f));
+            Assert.AreEqual(new Vector3(1.5f, 1, 0f), e.CurrentPosition);
+        }
+        [Test]
+        public void CurrentPointWithGoingToEndPoint() {
+            path.points = new List<Vector3>() {Vector3.zero, Vector3.right, Vector3.right + Vector3.up, Vector3.right + Vector3.up + Vector3.right};
+            var e = path.GetEnumerator();
+            Assert.AreEqual(Vector3.zero, e.CurrentPosition);
+            Assert.IsTrue(e.GoForward(.5f));
+            Assert.AreEqual(Vector3.right * 0.5f, e.CurrentPosition);
+            Assert.IsFalse(e.GoForward(4f));
+            Assert.AreEqual(new Vector3(2f, 1, 0f), e.CurrentPosition);
+        }
+        [Test]
+        public void MoveForwardAndMoveToNextPoint() {
+            path.points = new List<Vector3>() {Vector3.zero, Vector3.right, Vector3.right * 2f};
+            var e = path.GetEnumerator();
+            Assert.AreEqual(Vector3.zero, e.CurrentPosition);
+            Assert.IsTrue(e.GoForward(.5f));
+            Assert.AreEqual(Vector3.right * 0.5f, e.CurrentPosition);
+            Assert.IsTrue(e.GoForward(.3f));
+            Assert.AreEqual(Vector3.right * 0.8f, e.CurrentPosition);
+            Assert.IsTrue(e.GoToNextPoint());
+            Assert.AreEqual(Vector3.right, e.CurrentPosition);
+        }
+        [Test]
+        public void MoveForwardAndMoveToPrevPoint() {
+            path.points = new List<Vector3>() {Vector3.zero, Vector3.right, Vector3.right * 2f};
+            var e = path.GetEnumerator();
+            Assert.AreEqual(Vector3.zero, e.CurrentPosition);
+            Assert.IsTrue(e.GoForward(.5f));
+            Assert.AreEqual(Vector3.right * 0.5f, e.CurrentPosition);
+            Assert.IsTrue(e.GoForward(.3f));
+            Assert.AreEqual(Vector3.right * 0.8f, e.CurrentPosition);
+            Assert.IsTrue(e.GoToPreviousPoint());
+            Assert.AreEqual(Vector3.zero, e.CurrentPosition);
+        }
+        [Test]
+        public void MoveToNextPointOnNearLastPoint() {
+            path.points = new List<Vector3>() {Vector3.zero, Vector3.right, Vector3.right * 2f};
+            var e = path.GetEnumerator();
+            Assert.AreEqual(Vector3.zero, e.CurrentPosition);
+            Assert.IsTrue(e.GoForward(1.5f));
+            Assert.AreEqual(Vector3.right * 1.5f, e.CurrentPosition);
+            Assert.IsTrue(e.GoToNextPoint());
+            Assert.AreEqual(Vector3.right * 2f, e.CurrentPosition);
+        }
         [Test] public void MoveForwardGiganticAmound() { Assert.Fail(); }
         [Test] public void MoveForwardNormalAmound() { Assert.Fail(); }
         [Test] public void MoveBackwardGiganticAmound() { Assert.Fail(); }
