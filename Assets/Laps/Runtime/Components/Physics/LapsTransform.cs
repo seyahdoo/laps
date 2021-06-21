9bfcd76997ea57f67edf111d225983c72f7e460d
Seyyid Ahmed Doğan <seyahdoo@gmail.com>
2021-06-21 11:40:39 +0300

    feat laps transform, enable disable slots

--- Assets/Laps/Runtime/Components/Physics/LapsTransform.cs
+++ Assets/Laps/Runtime/Components/Physics/LapsTransform.cs
@@ -4,18 +4,18 @@ namespace LapsRuntime {
     [AddComponentMenu("Laps/Physics/Laps Transform")]
     [LapsAddMenuOptions("Physics/Laps Transform")]
     public class LapsTransform : LapsComponent {
-        private Transform _transform;
-        public void Awake() {
-            _transform = transform;
-        }
         public override object HandleInput(int slotId, object parameter, LapsComponent eventSource) {
             switch (slotId) {
-                case 0:  return _transform;
+                case 0:  return transform;
+                case 1:  gameObject.SetActive(true); return null;
+                case 2:  gameObject.SetActive(false); return null;
                 default: return null;
             }
         }
         public override void GetInputSlots(SlotList slots) {
             slots.Add("transform", 0, null, typeof(Transform));
+            slots.Add("enable", 1);
+            slots.Add("disable", 2);
         }
     }
 }
--- Assets/Laps/Tests/EditModeTests/ComponentTests/LapsTransformTests.cs
+++ Assets/Laps/Tests/EditModeTests/ComponentTests/LapsTransformTests.cs
@@ -8,7 +8,10 @@ namespace LapsEditModeTests {
         [SetUp]
         public void Setup() {
             _lapsTransform = new GameObject().AddComponent<LapsTransform>();
-            _lapsTransform.Awake();
+        }
+        [TearDown]
+        public void Teardown() {
+            Object.DestroyImmediate(_lapsTransform.gameObject);
         }
         [Test]
         public void GetTransform() {
@@ -16,11 +19,25 @@ namespace LapsEditModeTests {
             Assert.AreEqual(_lapsTransform.transform, r);
         }
         [Test]
+        public void Enable() {
+            _lapsTransform.gameObject.SetActive(false);
+            _lapsTransform.HandleInput(1, null, null);
+            Assert.AreEqual(true, _lapsTransform.gameObject.activeSelf);
+        }
+        [Test]
+        public void Disable() {
+            _lapsTransform.gameObject.SetActive(true);
+            _lapsTransform.HandleInput(2, null, null);
+            Assert.AreEqual(false, _lapsTransform.gameObject.activeSelf);
+        }
+        [Test]
         public void Slots() {
             var slots = new SlotList();
             _lapsTransform.GetInputSlots(slots);
-            Assert.AreEqual(1, slots.Count);
+            Assert.AreEqual(3, slots.Count);
             Assert.AreEqual(new LogicSlot("transform", 0, null, typeof(Transform)), slots[0]);
+            Assert.AreEqual(new LogicSlot("enable", 1), slots[1]);
+            Assert.AreEqual(new LogicSlot("disable", 2), slots[2]);
             slots.Clear();
             _lapsTransform.GetOutputSlots(slots);
             Assert.AreEqual(0, slots.Count);
